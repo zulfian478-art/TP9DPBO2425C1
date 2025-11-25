8c8e224853e1685be2808776e38d8b243596c7cd
Njul478 <zulfian478@upi.edu>
2025-11-25 20:38:10 +0700

    Add files via upload

---
+++ mvp_db.sql
@@ -0,0 +1,117 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 25, 2025 at 02:36 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `mvp_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jadwal`
+--
+
+CREATE TABLE `jadwal` (
+  `id` int(11) NOT NULL,
+  `seri` varchar(100) NOT NULL,
+  `tanggal` date NOT NULL,
+  `lokasi` varchar(100) NOT NULL,
+  `jenis_lomba` varchar(50) NOT NULL,
+  `jumlah_lap` int(11) DEFAULT NULL,
+  `durasi_menit` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `jadwal`
+--
+
+INSERT INTO `jadwal` (`id`, `seri`, `tanggal`, `lokasi`, `jenis_lomba`, `jumlah_lap`, `durasi_menit`, `created_at`, `updated_at`) VALUES
+(1, 'GP Australia', '2025-03-17', 'Melbourne', 'Sprint', 58, 120, '2025-11-25 03:49:40', '2025-11-25 03:49:40'),
+(2, 'GP Bahrain', '2025-03-24', 'Sakhir', 'Full Race', 57, 110, '2025-11-25 03:49:40', '2025-11-25 03:49:40'),
+(3, 'GP Monaco', '2025-05-26', 'Monaco', 'Full Race', 78, 105, '2025-11-25 03:49:40', '2025-11-25 03:49:40');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembalap`
+--
+
+CREATE TABLE `pembalap` (
+  `id` int(11) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `tim` varchar(255) NOT NULL,
+  `negara` varchar(255) NOT NULL,
+  `poinMusim` int(11) DEFAULT 0,
+  `jumlahMenang` int(11) DEFAULT 0
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `pembalap`
+--
+
+INSERT INTO `pembalap` (`id`, `nama`, `tim`, `negara`, `poinMusim`, `jumlahMenang`) VALUES
+(1, 'Lewis Hamilton', 'Mercedes', 'United Kingdom', 347, 11),
+(2, 'Max Verstappen', 'Red Bull', 'Netherlands', 335, 10),
+(3, 'Valtteri Bottas', 'Mercedes', 'Finland', 203, 2),
+(4, 'Sergio Perez', 'Red Bull', 'Mexico', 190, 1),
+(5, 'Carlos Sainz', 'Ferrari', 'Spain', 150, 0),
+(6, 'Daniel Ricciardo', 'McLaren', 'Australia', 115, 1),
+(7, 'Charles Leclerc', 'Ferrari', 'Monaco', 95, 0),
+(8, 'Lando Norris', 'McLaren', 'United Kingdom', 88, 0),
+(9, 'Pierre Gasly', 'AlphaTauri', 'France', 75, 0),
+(10, 'Fernando Alonso', 'Alpine', 'Spain', 65, 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `pembalap`
+--
+ALTER TABLE `pembalap`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `jadwal`
+--
+ALTER TABLE `jadwal`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `pembalap`
+--
+ALTER TABLE `pembalap`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
